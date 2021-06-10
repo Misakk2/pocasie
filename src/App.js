5cff471bd7ce6119658ad9e31fc060c0c21cdad8
Michal Smriga <michal.smriga@gmail.com>
2021-06-10 02:02:29 +0200

    pocasie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import "./styles/index.css"
 
 function App() {
   return (
     <div className="App">
-
+      <div className='container'>
+        <p className='datum'>Hello motherfucker</p>
+      </div>
     </div>
   );
 }
