8eadd77c92e94fed825158b2f782852cf10322ac
Michal Smriga <michal.smriga@gmail.com>
2021-06-15 15:19:07 +0200

    pocasie

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import { Weather } from './components/Body/pocasie/Weather';
 import { Search } from './components/Body/search/Search';
 import { Header } from './components/Header/Header';
 import { CurrentContext } from './context/CurrentContext';
+import { useTransition, animated } from 'react-spring';
 
 function App() {
   const { currentCity } = useContext(CurrentContext);
+  const transition = useTransition(currentCity.city == undefined, {
+    from: { y: 800, opacity: 0 },
+    enter: { y: 0, opacity: 1 },
+    leave: { y: 800, opacity: 0 },
+  });
+  const weatherTransition = useTransition(currentCity.city !== undefined, {
+    from: { y: 800, opacity: 0 },
+    enter: { y: 0, opacity: 1 },
+    leave: { y: 800, opacity: 0 },
+  })
 
   return (
     <div className="container">
@@ -13,11 +24,13 @@ function App() {
         <Header />
       </div>
       <div className="pocasie">
-        {currentCity.city ?
-          (<Weather />)
-          :
-          (<Search />)
-        }
+        {transition((style, item) =>
+          item ? <animated.div style={style}> <Search key="search" /></animated.div> : ""
+        )}
+        {weatherTransition((style, item) =>
+          item ? <animated.div style={style}> <Weather /></animated.div> : ""
+        )}
+
       </div>
     </div>
   );
